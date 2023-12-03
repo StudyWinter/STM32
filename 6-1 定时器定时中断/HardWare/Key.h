23f121f158564e4b80f7f270797b663e4d2f0cde
StudyWinter <1394645503@qq.com>
2023-12-03 15:55:57 +0800

    TIM定时器之 6-1 定时器中断

---
+++ 6-1 定时器定时中断/HardWare/Key.h
@@ -0,0 +1,10 @@
+#ifndef __KEY_H__
+#define __KEY_H__
+
+void KEY_Init(void);				// 按键初始化
+uint8_t KEY_GetNum(void);			// 读取按键的值
+
+
+
+
+#endif
---
+++ 6-1 定时器定时中断/Objects/core_cm3.d
@@ -0,0 +1,2 @@
+.\objects\core_cm3.o: Start\core_cm3.c
+.\objects\core_cm3.o: D:\Software\coder tool\keil\install\ARM\ARMCC\Bin\..\include\stdint.h
